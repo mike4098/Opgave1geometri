fd1c74875ed46119ce9e908558570298119c813f
mike4098 <69966597+mike4098@users.noreply.github.com>
2021-02-23 09:18:41 +0100

    fatastatstsst

---
+++ Opgave1OOP/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opgave1OOP
+{
+    class SetNget
+    {  
+      
+        public int Perimeter        
+        {
+            get {return Perimeter; }
+            set { Perimeter = 1; }
+        }
+    }
+  
+}
+
---
+++ Opgave1OOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Opgave1OOP
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            //Geometri n = new Geometri();
+
+            SetNget s = new SetNget();
+            s.Perimeter = 7;
+            Console.WriteLine(s.Perimeter);
+
+            Console.ReadKey();
+        }
+          
+
+
+    }
+ 
+}  
+  
+
